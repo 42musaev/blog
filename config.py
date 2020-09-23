0d60291adafddc04575c92209fe562b7eb57f5a9
Askhab Musaev <42musaev@gmail.com>
2020-09-23 10:17:03 +0300

    include db, add model Post

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 class Configuration(object):
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://flask_user:1234@localhost/flask_db"
---
+++ posts/models.py
@@ -0,0 +1,26 @@
+from app import db
+from datetime import datetime
+import re
+
+
+def slugify(s):
+    patterns = r'[^\w+]'
+    return re.sub(patterns, '-', s).lower()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    slug = db.Column(db.String(256), unique=True)
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    def generate_slug(self):
+        if self.title:
+            self.slug = slugify(self.title)
+
+    def __repr__(self):
+        return f'<Post id: {self.id}, title: {self.title}>'
